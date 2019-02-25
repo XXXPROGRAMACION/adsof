ba92e7b5b4c0426d27acf748833a5ec79ee64dc0
Alejandro Pascual <alejandropascualpozo@gmail.com>
2019-02-25 11:57:05 +0100

    Creados Camino y Posada y comenzado Explorador

---
+++ P3/doc/es/uam/eps/ads/p3/Camino.java
@@ -0,0 +1,105 @@
+package es.uam.eps.ads.p3;
+
+/**
+ * <p>Esta clase permite trabajar con caminos.</p>
+ * 
+ * @author <p>Alejandro Pascual (alejandro.pascualp@estudiante.uam.es)<br>
+ *         Víctor Yrazusta (victor.yrazusta@estudiante.uam.es)</p>
+ */
+public class Camino {
+    private static int costeAcumulado;
+
+    private Posada origen;
+    private Posada destino;
+    private int coste;
+
+    /**
+     * Inicializa un camino dadas sus características.
+     * @param origen Posada de la que parte el camino
+     * @param destino Posada a la que llega el camino
+     * @param coste Coste energético de recorrer el camino
+     */
+    public Camino(Posada origen, Posada destino, int coste) {
+        this.origen = origen;
+        this.destino = destino;
+        if (coste >= 1) {
+            this.coste = coste;
+        } else {
+            this.coste = 1;
+        }
+        costeAcumulado += this.coste;
+    }
+    
+    /**
+     * Modifica el destino y el coste del camino
+     * @param destino Nueva posada a la que llega el camino
+     * @param coste Nuevo coste enérgetico de recorrer el camino
+     */
+    public void cambiarDestino(Posada destino, int coste) {
+        this.destino = destino;
+        costeAcumulado -= this.coste;
+        if (coste >= 1) {
+            this.coste = coste;
+        } else {
+            this.coste = 1;
+        }
+        costeAcumulado += this.coste;
+    }
+
+    /**
+     * Devuelve el coste acumulado.
+     * @return Coste acumulado de todas las instancias del camino
+     */
+    public static int getCosteAcumulado() {
+        return costeAcumulado;
+    }
+
+    /**
+     * Devuelve el origen.
+     * @return Posada de la que parte el camino
+     */
+    public Posada getOrigen() {
+        return origen;
+    }
+
+    /**
+     * Devuelve el destino.
+     * @return Posada a la que llega el camino
+     */
+    public Posada getDestino() {
+        return destino;
+    }
+
+    /**
+     * Devuelve el coste.
+     * @return Coste energético de recorrer el camino
+     */
+    public int getCoste() {
+        return coste;
+    }
+
+    /**
+     * Devuelve el coste especial.
+     * @return Coste especial de recorrer el camino, 0 en este caso
+     */
+    public int getCosteEspecial() {
+        return 0;
+    }
+
+    /**
+     * Devuelve el coste real.
+     * @return Coste real de recorrer el camino, suma del coste y el coste 
+     *         especial
+     */
+    public int getCosteReal() {
+        return getCoste()+getCosteEspecial();
+    }
+
+    /**
+     * Devuelve en un String la información de la clase.
+     * @return String indicando el origen, el coste y el destino
+     */
+    public String toString() {
+        return "(" + origen + "--" + coste + "-->" + destino + ")";
+    }
+}
---
+++ P3/doc/es/uam/eps/ads/p3/Posada.java
@@ -0,0 +1,113 @@
+package es.uam.eps.ads.p3;
+
+import java.util.Objects;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * <p>Esta clase permite trabajar con posadas.</p>
+ * 
+ * @author <p>Alejandro Pascual (alejandro.pascualp@estudiante.uam.es)<br>
+ *         Víctor Yrazusta (victor.yrazusta@estudiante.uam.es)</p>
+ */
+public class Posada {
+    private String nombre;
+    private int energiaRecuperada;
+    private List<Camino> caminos = new ArrayList<>();
+
+    /**
+     * Inicializa una posada dado su nombre. Su energía recuperada en la posada
+     * es por defecto 2.
+     * @param nombre Nombre de la posada
+     */
+    public Posada(String nombre) {
+        this.nombre = nombre;
+        energiaRecuperada = 2;
+    }
+
+    /**
+     * Inicializa una posada dados su nombre y energía recuperada.
+     * @param nombre Nombre de la posada
+     * @param energiaRecuperada Energía recuperada en la posada
+     */
+    public Posada(String nombre, int energiaRecuperada) {
+        this(nombre);
+        this.energiaRecuperada = energiaRecuperada;
+    }
+
+    /**
+     * Añade un camino a la lista de caminos de la posada.
+     * @param camino Camino a añadir
+     * @return "true" en caso de que se haya añadido el camino correctamente, o
+     *         "false", en caso de que no se haya añadido, ya sea porque el 
+     *         origen del camino no es la posada, o porque el origen y el
+     *         destino de la posada sea el mismo
+     */
+    public boolean addCamino(Camino camino) {
+        if (Objects.equals(this, camino.getOrigen()) || 
+            !Objects.equals(this, camino.getDestino())) {
+            return false;
+        }
+        caminos.add(camino);
+        return true;
+    }
+
+    /**
+     * Devuelve el nombre de la posada.
+     * @return Nombre de la posada
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Devuelve la energía recuperada en la posada
+     * @return Energía recuperada en la posada
+     */
+    public int getEnergiaRecuperada() {
+        return energiaRecuperada;
+    }
+
+    /**
+     * Devuelve el camino i-ésimo en orden de agregación.
+     * @param posicion Posición del camino en orden de agregación
+     * @return Camino en la posición especificada
+     */
+    public Camino getCamino(int posicion) {
+        return caminos.get(posicion);
+    }
+    
+    /**
+     * Devuelve el camino que llega a la posada pasada por argumento.
+     * @param posada Posada destino del camino a buscar
+     * @return El camino que conecta las dos posadas, o "null" en caso de que no
+     *         exista un camino que conecte las dos posadas de esta manera
+     */
+    public Camino getCamino(Posada posada) {
+        for (Camino camino : caminos) {
+            if (Objects.equals(camino.getDestino(), posada)) {
+                return camino;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Recoge la información de la instancia en una cadena de caracteres.
+     * @return String con el nombre, la energía recuperada y los caminos que 
+     *         parten de ella
+     */
+    public String toString() {
+        return nombre + "(" + energiaRecuperada + ")" + caminos;
+    }
+    
+    /**
+     * Compara dos posadas
+     * @param posada Posada con la que comparar la instacia
+     * @return "true" en caso de que las posadas sean iguales, o "false" en 
+     *         caso contrario
+     */
+    public boolean equals(Posada posada) {
+        return Objects.equals(nombre, posada.getNombre());
+    }
+}
