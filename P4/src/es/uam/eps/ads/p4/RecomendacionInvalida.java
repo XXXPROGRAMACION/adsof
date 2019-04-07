f91f4d0142497fe494d31ecc90bcaf6c0434e836
Pascualex <alejandropascualpozo@gmail.com>
2019-04-07 23:02:24 +0200

    Serialización para recomendación inválida

--- P4/src/es/uam/eps/ads/p4/RecomendacionInvalida.java
+++ P4/src/es/uam/eps/ads/p4/RecomendacionInvalida.java
@@ -4,5 +4,5 @@ package es.uam.eps.ads.p4;
  * Indica que se ha intentado producir una recomendación invalida.
  */
 public class RecomendacionInvalida extends Exception {
-    
+    public static final long serialVersionUID = 0;
 }
