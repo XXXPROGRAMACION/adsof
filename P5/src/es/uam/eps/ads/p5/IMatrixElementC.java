10a21d602d1363a99173273e2f9775378a73276a
Alejandro Pascual <alejandropascualpozo@gmail.com>
2019-04-08 12:12:14 +0200

    IMatrixElement implementado

---
+++ P5/src/es/uam/eps/ads/p5/IMatrixElementC.java
@@ -0,0 +1,46 @@
+package es.uam.eps.ads.p5;
+
+public class IMatrixElementC<T> implements IMatrixElement<T> {
+    private int i;
+    private int j;
+    private T element;
+
+    public IMatrixElementC(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    @Override
+    public int getI() {
+        return i;
+    }
+
+    @Override
+    public int getJ() {
+        return j;
+    }
+
+    @Override
+    public T getElement() {
+        return element;
+    }
+
+    @Override
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        if (element != null) {
+            return "(" + i + ", " + j + ") -> " + element;
+        } else {
+            return "(" + i + ", " + j + ") -> empty";
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return element.equals(obj);
+    }
+}
